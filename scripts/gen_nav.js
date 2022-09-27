976f550abd44d74155d1e24714097e50deb94f7a
Alexander Betancourt <40041601+XOR-SABER@users.noreply.github.com>
2022-09-27 03:17:32 -0700

    Progressss

---
+++ scripts/gen_nav.js
@@ -0,0 +1,107 @@
+var main_nav = '' + 
+'    <nav class="navbar navbar-expand-lg navbar-dark bg-dark py-0">' + 
+'        <div class = "container">' + 
+'        <a class="navbar-brand d-inline" href="#" >' + 
+'            <img src="/imgs/logo.svg" style="width: 12%;" class="">' + 
+'            BetanVault' + 
+'            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarS" aria-controls="navbarS" aria-expanded="true" aria-label="Toggle navigation">' + 
+'                <span class="navbar-toggler-icon"></span>' + 
+'            </button>' + 
+'        </a>' + 
+'            <div class="collapse navbar-collapse" id="navbarS">' + 
+'                <ul class="navbar-nav ms-auto">' + 
+'                    <li class="nav-item active px-1">' + 
+'                        <a href="index.html#" class="nav-link">Home</a>' + 
+'                    </li>' + 
+'                    <li class="nav-item dropdown px-1">' + 
+'                        <a class="nav-link dropdown-toggle"  role="button" data-bs-toggle="dropdown" aria-expanded="false">' + 
+'                            About_me' + 
+'                        </a>' + 
+'                        <ul class="bg-dark dropdown-menu">' + 
+'                            <li><a class="dropdown-item text-light" href="/index.html#AboutMe">Description</a></li>' + 
+'                            <li><a class="dropdown-item text-light" href="/menu_about_me.html#">Menu</a></li>' + 
+'                            <li><a class="dropdown-item text-light" href="/more_about_me.html#">More About Me</a></li>' + 
+'                            <li><a class="dropdown-item text-light" href="/menu_about_me.html#SCCCD">State Center Community College District</a>' + 
+'                                <ul class="bg-dark dropdown-menu dropdown-submenu">' + 
+'                                    <li><a class="dropdown-item text-light" href="#">Reedley College</a></li>' + 
+'                                    <li><a class="dropdown-item text-light" href="#">Fresno City College</a></li>' + 
+'                                    <li><a class="dropdown-item text-light" href="#">Clovis Community College</a></li>' + 
+'                                </ul>' + 
+'                            </li>' + 
+'                            <li><a class="dropdown-item text-light" href="/menu_about_me.html#Artcles">Working as a Tutor</a>' + 
+'                                <ul class="bg-dark dropdown-menu dropdown-submenu">' + 
+'                                    <li><a class="dropdown-item text-light" href="#">Reedley College Math Center</a></li>' + 
+'                                    <li><a class="dropdown-item text-light" href="#">Fresno City College Math Center</a></li>' + 
+'                                </ul>' + 
+'                            </li>' + 
+'                            <li><a class="dropdown-item text-light" href="#">CSU Monterey and beyond</a></li>' + 
+'                            <li><a class="dropdown-item text-light" href="/menu_about_me.html#Coursework">Coursework</a>' + 
+'                                <ul class="bg-dark dropdown-menu dropdown-submenu">' + 
+'                                    <li><a class="dropdown-item text-light" href="/csci-40.html#">CSCI-40</a></li>' + 
+'                                    <li><a class="dropdown-item text-light" href="/csci-41.html#">CSCI-41</a></li>' + 
+'                                    <li><a class="dropdown-item text-light" href="/csci-26.html#">CSCI-26</a></li>' + 
+'                                    <li><a class="dropdown-item text-light" href="/csci-45.html#">CSCI-45</a></li>' + 
+'                                </ul>' + 
+'                            </li>' + 
+'                          </ul>' + 
+'                      </li>' + 
+'                      <li class="nav-item dropdown">' + 
+'                        <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">' + 
+'                            Incoherent_Ramblings' + 
+'                        </a>' + 
+'                        <ul class="bg-dark dropdown-menu">' + 
+'                            <li><a class="dropdown-item text-light" href="/index.html#Blog">Description</a></li>' + 
+'                            <li><a class="dropdown-item text-light" href="/menu_blog.html#">Menu</a></li>' + 
+'                            <li><a class="dropdown-item text-light" href="#">Why Graph theory is extremely useful!</a></li>' + 
+'                            <li><a class="dropdown-item text-light" href="#">Hash tables are overpowered!</a></li>' + 
+'                            <li><a class="dropdown-item text-light" href="#">Nixonverse is the best Youtube series in a while.</a></li>' + 
+'                            <li><a class="dropdown-item text-light" href="#">Making Video games is really hard..</a></li>' + 
+'                            <li><a class="dropdown-item text-light" href="#">Why a professor and not a professonal?</a></li>' + 
+'                            <li><a class="dropdown-item text-light" href="#">Sunsets: tying-up loose ends.</a></li>' + 
+'                          </ul>' + 
+'                      </li>' + 
+'                      <li class="nav-item dropdown">' + 
+'                        <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">' + 
+'                            C++_Projects' + 
+'                        </a>' + 
+'                        <ul class="bg-dark dropdown-menu">' + 
+'                            <li><a class="dropdown-item text-light" href="/index.html#CC">Description</a></li>' + 
+'                            <li><a class="dropdown-item text-light" href="/menu_cpp_projects.html#">Menu</a></li>' + 
+'                            <li><a class="dropdown-item text-light" href="#">Lyric Generator</a></li>' + 
+'                            <li><a class="dropdown-item text-light" href="#">Schedule Builder part 1: Graphs</a></li>' + 
+'                            <li><a class="dropdown-item text-light" href="#">Line rasterization</a></li>' + 
+'                            <li><a class="dropdown-item text-light" href="#">Hash Report</a></li>' + 
+'                        </ul>' + 
+'                      </li>' + 
+'                    <li class="nav-item dropdown">' + 
+'                        <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">' + 
+'                            Other_Projects' + 
+'                        </a>' + 
+'                        <ul class="bg-dark dropdown-menu">' + 
+'                            <li><a class="dropdown-item text-light" href="/index.html#Proj">Description</a></li>' + 
+'                            <li><a class="dropdown-item text-light" href="/menu_other_projects.html#">Menu</a></li>' + 
+'                            <li><a class="dropdown-item text-light" href="#">Tetris.js</a></li>' + 
+'                            <li><a class="dropdown-item text-light" href="#">Cubed^2</a></li>' + 
+'                            <li><a class="dropdown-item text-light" href="/asm_complier.html#">ASM Complier</a></li>' + 
+'                            <li><a class="dropdown-item text-light" href="#">Cheem\'s tweaks</a></li>' + 
+'                            <li><a class="dropdown-item text-light" href="#">Python Convex hulls</a></li>' + 
+'                            <li><a class="dropdown-item text-light" href="#">ACUnitTest.h</a></li>' + 
+'                          </ul>' + 
+'                      </li>' + 
+'                    <li class="nav-item">' + 
+'                        <a href="#Contact" class="nav-link">Contact</a>' + 
+'                    </li>' + 
+'                    <li class="nav-item">' + 
+'                        <a href="https://github.com/XOR-SABER" class="nav-link">' + 
+'                            <i class="img-fluid bi bi-github"></i>' + 
+'                        </a>' + 
+'                    </li>' + 
+'                </ul>' + 
+'            </div>' + 
+'        </div>' + 
+'    </nav>' + 
+'';
+function gen_nav() {
+    document.getElementById("main_nav").innerHTML = main_nav;
+}
+gen_nav();
